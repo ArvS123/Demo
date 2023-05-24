4d578f0a29bd1c62519983bd9296363956100bcd
ArvS123 <134562230+ArvS123@users.noreply.github.com>
2023-05-24 19:48:32 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var paddle;
+function setup() {    
+  createCanvas(1000,1000);
+
+  paddle = createSprite(150, 200, 100, 25);
+}
+
+function draw() 
+{
+  background("black");
+
+  drawSprites();
+
+}
+
+
+
+
+
